ccd6c8efe075f2e8b2645ffad29ff0b9218a60e1
U-m2n2 <dd305@snu.ac.kr>
2021-10-25 15:38:27 +0900

    first!

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "plugin:vue/essential",
+        "standard"
+    ],
+    "parserOptions": {
+        "ecmaVersion": 13,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+        "vue/html-closing-bracket-newline": ["error", {
+            "singleline": "never",
+            "multiline": "never"
+          }],
+          "vue/html-self-closing": ["error", {
+            "html": {
+              "void": "always",
+              "normal": "never",
+              "component": "always"
+            },
+            "svg": "always",
+            "math": "always"
+          }]
+    }
+};
